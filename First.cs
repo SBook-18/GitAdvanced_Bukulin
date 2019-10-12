1bc244aac546b78ddf294a8b1e1375217498b113
SBook-18 <56364504+SBook-18@users.noreply.github.com>
2019-10-12 13:28:56 +0200

    Update First.cs

--- First.cs
+++ First.cs
@@ -1,3 +1,5 @@
 Console.Write.Line("Client: Das ist die erste Zeile")
 Console.Write.Line("Client: Das ist die zweite Zeile")
 Console.Write.Line("Client: Das ist die dritte Zeile")
+Console.Write.Line("Github: Das ist die vierte Zeile")
+Console.Write.Line("Github: Das ist die fünfte Zeile")
